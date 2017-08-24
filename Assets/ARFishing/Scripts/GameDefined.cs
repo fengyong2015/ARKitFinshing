61dea523d71c147780b3590a2e7ef4da2423d86d
fengyong <fengyong@linqsoft.com>
2017-08-24 18:46:43 +0800

    - modify fish

--- Assets/ARFishing/Scripts/GameDefined.cs
+++ Assets/ARFishing/Scripts/GameDefined.cs
@@ -1,19 +1,34 @@
 ﻿public enum FinishType
 {
 	None = 0,
-	扯旗鱼 = 1,
-	灯笼鱼 = 2,
-	电鳗鱼 = 3,
-	帝鲶鱼 = 4,
-	鲂鱼 = 5,
-	海龟 = 6,
-	河豚 = 7,
-	剑鱼 = 8,
-	金枪鱼 = 9,
-	鲨鱼 = 10,
-	狮子鱼 = 11,
-	天使鱼 = 12,
-	小丑鱼 = 13,
-	小黄鱼 = 14,
-	小鲤鱼 = 15
+	///扯旗鱼
+	cheqiyu = 1,
+	///灯笼鱼
+	denglongyu = 2,
+	///电鳗鱼
+	dianmanyu = 3,
+	///帝鲶鱼
+	dinianyu = 4,
+	///鲂鱼
+	fangyu = 5,
+	///海龟
+	haigui = 6,
+	///河豚 
+	hetun = 7,
+	///剑鱼 
+	jianyu = 8,
+	///金枪鱼 
+	jinqingyu = 9,
+	///鲨鱼 
+	shayu = 10,
+	///狮子鱼
+	shiziyu = 11,
+	///天使鱼 
+	tainshiyu = 12,
+	///小丑鱼
+	xiaochouyu = 13,
+	///小黄鱼
+	xiaohuangyu = 14,
+	///小绿鱼
+	xiaolvyu = 15
 }
