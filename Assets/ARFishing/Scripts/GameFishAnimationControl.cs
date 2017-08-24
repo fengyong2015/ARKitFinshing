f92c6b37a1fbd1cda25ea06c82c1d8a77b4a8a20
fengyong <fengyong@linqsoft.com>
2017-08-24 17:48:25 +0800

    add anim

---
+++ Assets/ARFishing/Scripts/GameFishAnimationControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FinishAnimState
+{
+	None,
+	Idle,
+	Move,
+	Die
+
+}
+
+public class GameFishAnimationControl : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/ARFishing/Scripts/TestMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.RotateAround (new Vector3 (0, 1, 0), Vector3.up, 30 * Time.deltaTime);
+	}
+}
