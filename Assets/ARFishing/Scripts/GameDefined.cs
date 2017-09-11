821d2d463493980a574229b16cc09abc81d26771
fengyong <fengyong@linqsoft.com>
2017-09-11 20:17:02 +0800

    - modify story model

--- Assets/ARFishing/Scripts/GameDefined.cs
+++ Assets/ARFishing/Scripts/GameDefined.cs
@@ -31,3 +31,106 @@
 	xiaohuangyu = 14,
 }
 
+
+public static class FishTypeExtensions
+{
+	public static string FishInfo (this FishType pFishType)
+	{
+		string tInfo = string.Empty;
+		switch (pFishType) {
+		case FishType.cheqiyu:
+			tInfo = "扯旗鱼属于脂鲤科小型热带鱼，体型侧扁，尾鳍呈叉型，雄鱼的背鳍高耸飘逸，犹如一面迎风招展的旗帜，因而得名";
+			break;
+		case FishType.denglongyu:
+			tInfo = "灯笼鱼头大尾细，身体圆尾巴细，体表被有银灰色的薄鳞。在头部的前边，眼的附近，身体侧线下方和尾柄上，有排列成行或成群的圆形发光器。";
+			break;
+		case FishType.dianmanyu:
+			tInfo = "";
+			break;
+		case FishType.dinianyu:
+			tInfo = "";
+			break;
+		case FishType.fangyu:
+			tInfo = "";
+			break;
+		case FishType.haigui:
+			tInfo = "";
+			break;
+		case FishType.hetun:
+			tInfo = "";
+			break;
+		case FishType.jianyu:
+			tInfo = "";
+			break;
+		case FishType.jinqiangyu:
+			tInfo = "";
+			break;
+		case FishType.shayu:
+			tInfo = "";
+			break;
+		case FishType.shiziyu:
+			tInfo = "";
+			break;
+		case FishType.tianshiyu:
+			tInfo = "";
+			break;
+		case FishType.xiaochouyu:
+			tInfo = "";
+			break;
+		case FishType.xiaohuangyu:
+			tInfo = "";
+			break;
+		}
+		return tInfo;
+	}
+
+	public static string FishName (this FishType pFishType)
+	{
+		string tInfo = string.Empty;
+		switch (pFishType) {
+		case FishType.cheqiyu:
+			tInfo = "扯旗鱼";
+			break;
+		case FishType.denglongyu:
+			tInfo = "灯笼鱼";
+			break;
+		case FishType.dianmanyu:
+			tInfo = "电鳗";
+			break;
+		case FishType.dinianyu:
+			tInfo = "帝鲶鱼";
+			break;
+		case FishType.fangyu:
+			tInfo = "鲂鱼";
+			break;
+		case FishType.haigui:
+			tInfo = "海龟";
+			break;
+		case FishType.hetun:
+			tInfo = "河豚";
+			break;
+		case FishType.jianyu:
+			tInfo = "剑鱼";
+			break;
+		case FishType.jinqiangyu:
+			tInfo = "金枪鱼";
+			break;
+		case FishType.shayu:
+			tInfo = "鲨鱼";
+			break;
+		case FishType.shiziyu:
+			tInfo = "狮子鱼";
+			break;
+		case FishType.tianshiyu:
+			tInfo = "天使鱼";
+			break;
+		case FishType.xiaochouyu:
+			tInfo = "小丑鱼";
+			break;
+		case FishType.xiaohuangyu:
+			tInfo = "小黄鱼";
+			break;
+		}
+		return tInfo;
+	}
+}
--- Assets/ARFishing/Scripts/_Story/StorySceneManager.cs
+++ Assets/ARFishing/Scripts/_Story/StorySceneManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StorySceneManager : MonoBehaviour
@@ -10,6 +11,11 @@ public class StorySceneManager : MonoBehaviour
 
 	[SerializeField]
 	Transform m_ScreenCenter;
+
+	[SerializeField]
+	Text m_StoryTitle;
+	[SerializeField]
+	Text m_StoryInfo;
 
 	List<FishType> tFishTypes = new List<FishType> (System.Enum.GetValues (typeof(FishType))as FishType[]);
 
@@ -39,13 +45,11 @@ public class StorySceneManager : MonoBehaviour
 		}
 	}
 
-
 	GameFish mGameFish;
 
 	void CatchFish (Vector2 pScreenPoint)
 	{
 		mGameFish = null;
-
 		// 以摄像机所在位置为起点，创建一条向下发射的射线  
 		Vector3 tCameraPos = Camera.main.transform.position;
 		Vector3 tFarPos = Camera.main.ScreenToWorldPoint (new Vector3 (pScreenPoint.x, pScreenPoint.y, m_Trans.position.z));
@@ -55,6 +59,11 @@ public class StorySceneManager : MonoBehaviour
 			mGameFish = hit.collider.transform.parent.GetComponent<GameFish> ();
 			if (mGameFish != null) {
 				CanCatch = false;
+				m_StoryTitle.text = mGameFish.fishType.FishName ();
+				m_StoryTitle.gameObject.SetActive (true);
+				m_StoryInfo.text = mGameFish.fishType.FishInfo ();
+				m_StoryInfo.gameObject.SetActive (true);
+
 				StartCoroutine (ShowFinish ());
 			}
 		}
@@ -64,6 +73,12 @@ public class StorySceneManager : MonoBehaviour
 	{
 		mGameFish.mRatateMove.isShowing = true;
 		yield return new WaitForSeconds (5f);
+		m_StoryTitle.gameObject.SetActive (false);
+		m_StoryInfo.gameObject.SetActive (false);
+		mGameFish.mRatateMove.isShowing = false;
+		mGameFish = null;
+		CanCatch = true;
+
 	}
 
 	public void Back ()
